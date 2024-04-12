b74253ddddc6f7883cf6e8a0d671d6c433bf1db0
anindyamanna <anindyamanna@gmail.com>
2024-04-12 18:50:27 +0530

    reverted

--- bin/bin.js
+++ bin/bin.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
-var lzString = require("../libs/lz-string.js");
-var fs = require("fs");
+var lzString = require('../libs/lz-string.js');
+var fs = require('fs');
 
 if (process.argv.length < 3) {
-  console.error("Usage: lz-string <input_file>");
+  console.error('Usage: lz-string <input_file>');
   process.exit(1);
 }
-if (process.argv[2] === "-c") {
-  console.log(lzString.compressToEncodedURIComponent(process.argv[3]));
-} else if (process.argv[2] == "-d") {
-  console.log(lzString.decompressFromUTF16(process.argv[3]));
-}
+
+console.log(lzString.decompressFromUTF16(fs.readFileSync(process.argv[2], {
+  encoding: 'utf8',
+})));
+
